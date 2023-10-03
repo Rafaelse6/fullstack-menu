6265bfbc254366088a6047f24241f5e2613ded97
Rafael Santos <rafa.05125@gmail.com>
2023-10-02 21:06:31 -0300

    Initial homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { Card } from "./components/card/card";
+import { FoodData } from "./interface/FoodData";
 
 function App() {
-  return <div></div>;
+  const data: FoodData[] = [];
+  return (
+    <div className="container">
+      <h1>Menu</h1>
+      <div className="card-grid">
+        {data.map((foodData) => (
+          <Card
+            price={foodData.price}
+            title={foodData.title}
+            image={foodData.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
